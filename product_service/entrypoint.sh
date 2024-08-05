44901bf1e9a3a1d1ef0912026db8b130d2425ebc
jam4li <ziigm4@gmail.com>
2024-08-05 16:32:59 +0330

    first commit

---
+++ product_service/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+while ! nc -z mysql_db 3306 ; do
+    echo "Waiting for the MySQL Server"
+    sleep 3
+done
+
+python manage.py migrate
+python manage.py collectstatic --no-input
+python manage.py makemessages -l fa
+python manage.py compilemessages > /dev/null
+
+exec "$@"
